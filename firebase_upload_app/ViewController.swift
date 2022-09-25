5317a7b4cf11e77b322dc248a283fe71fddf37f1
Jesús Perea <jesus.perea@konfio.mx>
2022-09-25 17:59:42 -0500

    Initial Commit

---
+++ firebase_upload_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebase_upload_app
+//
+//  Created by Jesús Perea (Mobile Engineer) on 25/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
